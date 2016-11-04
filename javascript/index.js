c88bcf63bda1dcda96bfaceb283110b9cc76aa27
Jack dev <dev@jackmx.net.ua>
2016-11-04 15:48:37 +0200

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	// bxslider header main-page
+	$('.header-slider').bxSlider({
+		// auto: true,
+		// pause: 6000
+	});
+	// end bxslider header main-page
+
+	
+});
