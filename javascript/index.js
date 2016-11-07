49da1b3bf8e995fd494febcb4ca4349a6fa0945b
Jack dev <dev@jackmx.net.ua>
2016-11-07 17:57:10 +0200

    zminyv slider na bx

--- javascript/index.js
+++ javascript/index.js
@@ -3,24 +3,11 @@ $(document).ready(function(){
 	var windowWidth = $(window).width();
 
 // Header---Header---Header---Header---Header---Header---
+	$(".slider").bxSlider({
+		auto: true,
+		pause: 4000
+	});
 
-	// slider in header
-	var sliderImage = document.getElementById("main_image");
-	var imageArray = ["image/header/slider/img1.png", "image/header/slider/img2.png", "image/header/slider/img3.png", "image/header/slider/img4.png", "image/header/slider/img5.png"];
-	var imageIndex = 0;
-
-	$("#main_image").css("transition", "0.6s");
-
-	function changeImage() {
-		sliderImage.setAttribute("src", imageArray[imageIndex]);
-		imageIndex++;
-		if (imageIndex >= imageArray.length) {
-			imageIndex = 0;
-		}
-	}
-
-	setInterval(changeImage, 4000);
-	// end slider in header
 
 	// header-block margin
 		$(window).resize(function(){
@@ -34,10 +21,6 @@ $(document).ready(function(){
 			}
 		});
 	// end header-block margin
-
-	// header menu after-before
-	
-	// header menu after-before
 
 // Header---Header---Header---Header---Header---Header---
 });
