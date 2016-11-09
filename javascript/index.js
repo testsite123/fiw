5cf034744e168a64080a3044de66707e731b8f06
Jack dev <dev@jackmx.net.ua>
2016-11-09 13:15:26 +0200

    footer header and resizer

--- javascript/index.js
+++ javascript/index.js
@@ -22,13 +22,17 @@ $(document).ready(function(){
 		});
 	// end header-block margin
 
-	// $("#architecture").click(function(){
-	// 	$(".categories-description").children().eq(0).toggleClass("description-hidden");
-	// 	$(".categories-description").children().eq(1).toggleClass("description-hidden");
-	// 	$(".categories-description").children().eq(2).toggleClass("description-hidden");
-	// 	$(".categories-description").children().eq(3).toggleClass("description-hidden");
-	// });
+// Header---Header---Header---Header---Header---Header---
 
+// Portfolio---Portfolio---Portfolio---Portfolio---Portfolio---
+	$(".portfolio-resizer-text").click(function(){
+		$(".portfolio-resizer").css("display", "none");
+		$(".portfolio").css("height", "100%").css("overflow", "visible").css("padding-bottom", "50px");
+		$(window).trigger('resize').trigger('scroll');
+	});
+// Portfolio---Portfolio---Portfolio---Portfolio---Portfolio---
+
+// Services---Services---Services---Services---Services---
 	$(".categories-wrapper").children().click(function(){
 		for (var i = 0; i < $(".categories-description").children().length; i++) {
 			if( $(".categories-description").children().eq(i).hasClass($(this)[0].id + "-description") ) {
@@ -43,6 +47,12 @@ $(document).ready(function(){
 			}
 		}
 	});
+// Services---Services---Services---Services---Services---
 
-// Header---Header---Header---Header---Header---Header---
+// Contacts---Contacts---Contacts---Contacts---Contacts---
+	$(".parallax-layer").parallax({
+		imageSrc: 'image/parallax/parallax.png'
+	});
+// Contacts---Contacts---Contacts---Contacts---Contacts---
 });
+
